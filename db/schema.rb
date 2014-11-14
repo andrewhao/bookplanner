688331594ee0ec55cb603a1f3e95892eaaefc03e
Andrew Hao <ahao@blurb.com>
2014-11-13 19:05:37 -0800

    Update student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930185830) do
+ActiveRecord::Schema.define(version: 20141114030458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20140930185830) do
     t.integer  "classroom_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "students", ["classroom_id"], name: "index_students_on_classroom_id", using: :btree
