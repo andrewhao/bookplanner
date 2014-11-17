69d1574408dcd6789be158e72a51ee9a1852e697
Andrew Hao <ahao@blurb.com>
2014-11-16 16:44:52 -0800

    Adding book bag classroom associations

--- app/models/book_bag.rb
+++ app/models/book_bag.rb
@@ -1,3 +1,5 @@
 class BookBag < ActiveRecord::Base
   belongs_to :classroom
+
+  #validates :classroom, presence: true
 end
--- spec/controllers/book_bags_controller_spec.rb
+++ spec/controllers/book_bags_controller_spec.rb
@@ -23,7 +23,9 @@ describe BookBagsController do
   # This should return the minimal set of attributes required to create a valid
   # BookBag. As you add validations to BookBag, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { global_id: "1" } }
+  let(:valid_attributes) do
+    { global_id: "1", classroom_id: 2 }
+  end
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
