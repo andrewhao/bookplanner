c50d7a2542777105ac6b0d37de9890b4815fa60e
Andrew Hao <andrewhao@gmail.com>
2015-01-19 23:44:44 -0500

    Updating the PlanUpdater to try to work around the SQL index uniqueness constraints for now.

--- app/services/plan_updater.rb
+++ app/services/plan_updater.rb
@@ -1,5 +1,29 @@
 # Given a parameter hash from the request, we properly tear down, update,
 # or recreate assignment and period information.
 class PlanUpdater
+  attr_reader :plan, :params
+  delegate :period, to: :plan
 
+  def initialize(plan, params)
+    @plan = plan
+    @params = params
+  end
+
+  def update
+    #update_assignment!
+    ActiveRecord::Base.transaction do
+      plan.update(params)
+    end
+  end
+
+  def update_assignment!
+    attrs = params.delete(:assignments_attributes)
+    return if attrs.nil?
+    new_query = {}
+    attrs.values.each do |at|
+      id_hash = at.extract!("id")
+      new_query[id_hash["id"]] = at
+    end
+    Assignment.update new_query.keys new_query.values
+  end
 end
--- spec/services/plan_updater_spec.rb
+++ spec/services/plan_updater_spec.rb
@@ -2,5 +2,49 @@ require "spec_helper"
 
 describe PlanUpdater do
   let(:plan) { FactoryGirl.create :plan_with_assignments }
-  subject { described_class.new(plan) }
+  subject { described_class.new(plan, params) }
+  let(:period) { plan.period }
+  let(:assignments) { plan.assignments }
+
+  let(:params) do
+    {
+      period_attributes: {
+        name: new_name,
+        id: period.id
+      }
+    }
+  end
+
+  let(:new_name) { "Week Sixteen" }
+
+  describe "#update" do
+    context "field updates on Period" do
+      it "updates a period name given param input" do
+        expect {
+          subject.update
+        }.to change { period.reload.name }.to(new_name)
+      end
+    end
+
+    context "assignments that already exist" do
+      let(:params) do
+        attrs = {}
+        bag_ids = assignments.map(&:book_bag_id)
+        assignments.each_with_index do |a, i|
+          rotated_bag_idx = (i + 1) % assignments.length
+          attrs[i] = a.attributes.extract!("id", "student_id", "book_bag_id")
+          attrs[i]["book_bag_id"] = bag_ids[rotated_bag_idx]
+        end
+        {
+          assignments_attributes: attrs
+        }
+      end
+
+      it "allows swapping" do
+        expect {
+          subject.update
+        }.to_not raise_error
+      end
+    end
+  end
 end
